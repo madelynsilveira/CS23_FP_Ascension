10ef30968d0e91cc0dcf79bb2989c99db98d433c
Silas Reed <silas.reed@tufts.edu>
2023-10-29 14:26:26 -0400

    Created GameHandler Canvas Prefab

--- Ascension_3D/Assets/Scripts/GameHandler.cs
+++ Ascension_3D/Assets/Scripts/GameHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class GameHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -20,6 +20,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
+        // reset static vars
     }
 
     public void QuitGame() {
