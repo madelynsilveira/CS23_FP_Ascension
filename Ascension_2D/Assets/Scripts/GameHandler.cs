2961f98d74b646cd4d653890ac345ba84961e06b
Silas Reed <silas.reed@tufts.edu>
2023-10-29 21:44:13 -0400

    Fixed main menu and added scenes to build settings

--- Ascension_2D/Assets/Scripts/GameHandler.cs
+++ Ascension_2D/Assets/Scripts/GameHandler.cs
@@ -5,10 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
+    public Scene scene;
+    public GameObject mainMenu;
+    public GameObject winScene;
+    public GameObject loseScene;
     // Start is called before the first frame update
     void Start()
     {
+        scene = SceneManager.GetActiveScene();
+        Debug.Log("Active scene:" + scene.name);
         
+        mainMenu.SetActive(false);
+        winScene.SetActive(false);
+        loseScene.SetActive(false);
+
+        if (scene.name == "MainMenu") {
+            mainMenu.SetActive(true);
+        } else if (scene.name == "winScene") {
+            winScene.SetActive(true);
+        } else if (scene.name == "loseScene") {
+            loseScene.SetActive(true);
+        }
     }
 
     // Update is called once per frame
