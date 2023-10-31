4a31cc45106d8036d8cd845963d83875ff50ad7e
Madelyn Silveira <136256203+mjsilveira@users.noreply.github.com>
2023-10-31 14:13:51 -0400

    NPC that recognizes player.
    knows when it sees and knows to attack but haven't configured states yet.

--- Ascension_2D/Assets/Assets/MecanimAIDemo/NPC/NPC_scripts/Walking_State.cs
+++ Ascension_2D/Assets/Assets/MecanimAIDemo/NPC/NPC_scripts/Walking_State.cs
@@ -13,9 +13,10 @@ public class Walking_State : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
         NPC = GameObject.FindWithTag("NPC");
-        
+        RaycastHit2D ray = Physics2D.Raycast(NPC.transform.position, Vector2.down, 1f, LayerMask.GetMask("PlatformLayer"));
+        Debug.Log("collide = " + ray.collider.bounds);
+        Debug.DrawRay(NPC.transform.position, Vector2.down, Color.red); // Draw a debug ray to visualize the raycast
 
     }
 
@@ -26,13 +27,23 @@ public class Walking_State : StateMachineBehaviour
         timeUntilMove -= Time.deltaTime;
         timeUntilTurn -= Time.deltaTime;
 
-
+        // have we encountered the player?
         Vector3 NPCPos = NPC.transform.position;
+        if (PlayerWithin(NPCPos, 3f)) {
+            // TRANSITION TO FIND / FOLLOW PLAYER STATE
+            Debug.Log("I see the player!");
+            if (PlayerWithin(NPCPos, 1)) {
+                // TRANSITION TO ATTACK 
+                Debug.Log("ATTACK THE PLAYER");
+            }
+        }
+
+        // checking for edges
         if (timeUntilTurn <= 0 && !CheckRays(NPCPos.x, NPCPos.y, NPC_half_width + 1.3f)) {
             destination = new Vector3(NPCPos.x * -1, NPCPos.y, 0);
         }
         
-        // bool to see if the x value has already been reached
+        // bool to see if the x destination value has already been reached
         bool closeEnough = (Mathf.Abs(NPC.transform.position.x - destination.x) < .3);
 
         // Direct the NPC to a random x position
@@ -49,6 +60,11 @@ public class Walking_State : StateMachineBehaviour
         NPC.GetComponent<NPCController>().moveToLocation(transform);
     }
 
+    private bool PlayerWithin(Vector3 NPCPos, float distance) 
+    {
+        return (Physics2D.Raycast(NPCPos, Vector2.left, distance, LayerMask.GetMask("Player")).collider != null);
+    }
+
     // function returns 0 if both are collided, 1 if left collided, 2 if right collided
     // used to see if grounded on platform or near a ledge and must turn around
     private bool CheckRays(float NPCxPos, float NPCyPos, float offset) 
@@ -56,8 +72,9 @@ public class Walking_State : StateMachineBehaviour
         bool leftHit = (Physics2D.Raycast(new Vector3(NPCxPos - offset, NPCyPos, 0), Vector2.down, 1f).collider != null);
         bool rightHit = (Physics2D.Raycast(new Vector3(NPCxPos + offset, NPCyPos, 0), Vector2.down, 1f).collider != null);
 
-        return leftHit && rightHit;
-
+        if (leftHit && rightHit) {
+            return true;
+        }
         timeUntilTurn = 4;
         Debug.Log("different!");
         return false;
