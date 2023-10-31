db5144e71c224f1d93228630b8308a58c94caee2
Silas Reed <silas.reed@tufts.edu>
2023-10-31 10:22:39 -0400

    Implemented basic flying

---
+++ Ascension_2D/Assets/Scripts/PlayerFly.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFly : MonoBehaviour
+{
+      //public Animator anim;
+      public Rigidbody2D rb;
+      public float flyForce = 10f;
+      //public Transform feet;
+      //public LayerMask groundLayer;
+      //public LayerMask enemyLayer;
+      public bool canFly = false;
+      //public int flyTimer = 0;
+      public bool isAlive = true;
+      //public AudioSource FlySFX;
+
+      void Start(){
+            //anim = gameObject.GetComponentInChildren<Animator>();
+            rb = GetComponent<Rigidbody2D>();
+
+            // temporary
+            canFly = true;
+      }
+
+      void Update() {
+            // replace with flying ability check
+            // if ((IsGrounded()) || (jumpTimes <= 1)){
+            //       canJump = true;
+            // }  else if (jumpTimes > 1){
+            //       canJump = false;
+            // }
+
+           if ((Input.GetKeyDown("up")) && (canFly) && (isAlive == true)) {
+                  Fly();
+            }
+      }
+
+      public void Fly() {
+            //flyTimer += 1;
+            rb.velocity = Vector2.up * flyForce;
+            // anim.SetTrigger("Jump");
+            // JumpSFX.Play();
+
+            //Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+            //rb.velocity = movement;
+      }
+
+    //   public bool IsGrounded() {
+    //         Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 2f, groundLayer);
+    //         Collider2D enemyCheck = Physics2D.OverlapCircle(feet.position, 2f, enemyLayer);
+    //         if ((groundCheck != null) || (enemyCheck != null)) {
+    //               //Debug.Log("I am trouching ground!");
+    //               jumpTimes = 0;
+    //               return true;
+    //         }
+    //         return false;
+    //   }
+}
