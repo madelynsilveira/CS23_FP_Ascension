fb8e665e9732afdf4f681e0d19136e6d2d5d863f
Silas Reed <silas.reed@tufts.edu>
2023-11-01 23:47:54 -0400

    Made camera follow player

---
+++ Ascension_2D/Assets/Scripts/CameraFollow2DLERP.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2DLERP : MonoBehaviour
+{
+    public GameObject target;
+    public float camSpeed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 pos = Vector2.Lerp ((Vector2) transform.position, (Vector2) target.transform.position,
+                                    camSpeed * Time.fixedDeltaTime);
+        transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+    }
+}
