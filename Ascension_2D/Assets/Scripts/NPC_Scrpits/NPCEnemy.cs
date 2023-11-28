c42454344a9e03eee8af4d93323735cf5f48b326
Silas Reed <silas.reed@tufts.edu>
2023-11-28 12:46:39 -0500

    Removed some debug printing

--- Ascension_2D/Assets/Scripts/NPC_Scrpits/NPCEnemy.cs
+++ Ascension_2D/Assets/Scripts/NPC_Scrpits/NPCEnemy.cs
@@ -41,12 +41,10 @@ public class NPCEnemy : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator anim, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log ("updating state");
         Vector3 NPCPos = NPC.transform.position;
         timeSinceLastEdge -= Time.deltaTime;
 
         if ((timeSinceLastEdge <= 0 ) && (anim.GetBool("npc_prowling"))) {
-            Debug.Log("checking edges");
             checkEdges(NPCPos);
             //checkEdges(transform.position);
         }
@@ -78,7 +76,6 @@ public class NPCEnemy : StateMachineBehaviour
                 // }
             }
         } else {
-            Debug.Log ("can't find player, prowling");
             anim.SetBool("npc_prowling", true);
             anim.SetBool("npc_pursuing", false);
             anim.SetBool("npc_attacking", false);
@@ -119,7 +116,6 @@ public class NPCEnemy : StateMachineBehaviour
         // raycast down to the left and right to check for collision
         Vector3 posLeft = new Vector3(NPCPos.x - 1, NPCPos.y, NPCPos.z);
         Vector3 posRight = new Vector3(NPCPos.x + 1, NPCPos.y, NPCPos.z);
-        Debug.Log (posLeft + ", " + posRight);
         bool leftRay = (Physics2D.Raycast(posLeft, Vector2.down, 2, LayerMask.GetMask("Ground")).collider != null);
         bool rightRay = (Physics2D.Raycast(posRight, Vector2.down, 2, LayerMask.GetMask("Ground")).collider != null);
         // call function to change the direction
